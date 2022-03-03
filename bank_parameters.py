73fb10862fae96e9be057db385094143d5b40b23
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-03-03 07:06:23 -0500

    Create bank_parameters.py

---
+++ bank_parameters.py
@@ -0,0 +1,6 @@
+Business practices for banking
+
+1 Customers should be able to check their balances
+2 Customers should not ba able to overdraw their account
+3 Customers should not be able to put in non-numbers for their account
+4 Customer should not be able to put in strings for their account
