bbabdd889fcf2d391a08bb4e2d346397451edc5b
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-03-03 16:09:00 -0500

    Create customer_class_info.py

---
+++ customer_class_info.py
@@ -0,0 +1,8 @@
+class Customer:
+
+    def __init__(self, customer_id:int, account_id:int, first_name:str, last_name:str, address:str):
+        self.customer_id = customer_id
+        self.account_id = account_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
