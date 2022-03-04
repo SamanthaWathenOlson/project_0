54860d129be2a083868e00beeedf725577973474
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-03-04 18:50:09 -0500

    Update customer_class_info.py

--- customer_class_info.py
+++ customer_class_info.py
@@ -1,8 +1,18 @@
-class Customer:
+lass Customer:
 
-    def __init__(self, customer_id:int, account_id:int, first_name:str, last_name:str, address:str):
+    def __init__(self, customer_id: int, account_id: int, first_name: str, last_name: str, address: str):
         self.customer_id = customer_id
         self.account_id = account_id
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
+
+
+"""
+What will our customer need to identify them?
+
+customers have names
+customer have address
+customers have account ids
+customer need a unique id 
+"""
