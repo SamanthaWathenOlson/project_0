07298c4cb2a130f107ff2f2c6b017014be5c00bf
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-03-07 17:03:24 -0500

    Update test_account_dao.py

--- test_account_dao.py
+++ test_account_dao.py
@@ -1,51 +1,58 @@
 from custom_exceptions.id_not_found import IdNotFound
-from data_access_layer.account_dao.account_dao_imp import CustomerDAOImp
-from objects.account_class_info import Customer
+from data_access_layer.account_dao.account_dao_imp import AccountDAOImp
+from objects.account_class_info import Account
 
-team_dao = TeamDAOImp()
-def test_create_team_success():
-    test_team = Team(0, "Trail Blazers", "Portland")
-    result = team_dao.create_team(test_team)
-    assert result.team_id != 0
+account_dao = AccountDAOImp()
+
+
+def test_create_account_success():
+    test_account = Account(0, 32867495, 34569847, 500.00)
+    result = account_dao.create_account(test_account)
+    assert result.account_id != 0
+
 
 def test_catch_non_unique_id():
-    test_team = Team(1, "Lakers", "L.A.")
-    result = team_dao.create_team(test_team)
-    assert result.team_id != 1
-
-def test_get_team_info_by_id_success():
-    result = team_dao.get_team_information_by_id(1)
-    assert result.team_id == 1
+    test_account = Account(1, 32867521, 34569947, 300.00)
+    result = account_dao.create_account(test_account)
+    assert result.account_id != 1
 
 
-def test_get_team_using_non_existant_id():
+def test_get_account_info_by_id_success():
+    result = account_dao.get_account_information_by_id(1)
+    assert result.account_id == 1
+
+
+def test_get_account_using_non_existent_id():
     try:
-        team_dao.get_team_information_by_id(0)
+        account_dao.get_account_information_by_id(0)
         assert False
     except IdNotFound as e:
-        assert str(e) == "No team matches the id given: please try again!"
+        assert str(e) == "No account matches the id given: please try again!"
 
-def test_update_team_by_id_success():
-    new_team_name = Team(1, "Batman", "Fort Wayne")
-    result = team_dao.update_team_by_id(new_team_name)
-    assert result.team_name == "Batman"
 
-def test_update_team_using_non_existant_id():
+def test_update_account_by_id_success():
+    previous_account_name = Account(1, 56792300, 34569947, 300.00)
+    result = account_dao.update_account_by_id(previous_account_name)
+    assert result.account_name == "Jennifer"
+
+
+def test_update_account_using_non_existent_id():
     try:
-        new_team_name = Team(0, "Batman", "Fort Wayne")
-        team_dao.update_team_by_id(new_team_name)
+        previous_account_name = Account(0, 56792300, 34569947, 120.69)
+        account_dao.update_account_by_id(previous_account_name)
         assert False
     except IdNotFound as e:
-        assert str(e) == "No team matches the id given: please try again!"
+        assert str(e) == "No account matches the id given: please try again!"
 
-def test_delete_team_by_id_success():
-    result = team_dao.delete_team_by_id(1)
+
+def test_delete_account_by_id_success():
+    result = account_dao.delete_account_by_id(1)
     assert result
 
 
-def test_delete_team_with_non_existant_id():
+def test_delete_account_with_non_existent_id():
     try:
-        team_dao.delete_team_by_id(0)
+        account_dao.delete_account_by_id(0)
         assert False
     except IdNotFound as e:
-        assert str(e) == "No team matches the id given: please try again!"
+        assert str(e) == "No account matches the i d given: please try again!"
