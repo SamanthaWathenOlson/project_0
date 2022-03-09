5d8393339f665792abe4e2e20436b7c94d59dd61
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-03-09 11:24:21 -0500

    Update bank_service_interface.py

--- bank_service_interface.py
+++ bank_service_interface.py
@@ -22,3 +22,23 @@ class BankServiceInterface(ABC):
     @abstractmethod
     def service_transfer_full_amount(self, value, account_id):
         pass
+
+       # create
+   @abstractmethod
+   def service_create_customer(self, customer: Customer) -> Customer:
+       pass
+
+   # read
+    @abstractmethod
+   def service_get_customer_information_by_id(self, customer_id: str) -> Customer:
+        pass
+
+    # update
+    @abstractmethod
+   def service_update_customer_by_id(self, customer: Customer) -> Customer:
+        pass
+
+    # delete
+    @abstractmethod
+   def service_delete_team_by_id(self, customer_id: int) -> bool:
+        pass
